f42b7be1efbe62bd16dc480698eefb33f0843988
zevdvlpr <zevdvlpr@gmail.com>
2021-06-02 14:26:59 -0300

    :sparkles: feat:  api animes

---
+++ src/pages/api/animes/popular.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { getPopular } from '../../../services/anime';
+
+export default async function Popular(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    const { currentPage, pageSize, maxPages } = req.query;
+
+    const pagination = {
+      currentPage: Number(currentPage) || 1,
+      pageSize: Number(pageSize) || 12,
+      maxPages: Number(maxPages) || 12,
+    };
+
+    const response = await getPopular(pagination);
+    
+    res.setHeader('X-Total-Count', response.totalItems);
+
+    return res.send(response);
+  }
+}
