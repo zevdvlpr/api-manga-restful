abe3b8b164a710f590a05d3cdaa3ab8570f3c435
zevdvlpr <zevdvlpr@gmail.com>
2021-06-02 14:25:27 -0300

    :sparkles: feat:  urls config

---
+++ src/config/urls.ts
@@ -0,0 +1,3 @@
+export const MANGA_LIVRE_SITE_URL = 'https://mangalivre.net';
+export const ANIME_TV_API_URL = 'https://appanimeplus.tk/api-animesbr-10.php';
+export const ANIME_TV_CDN_URL = 'https://cdn.appanimeplus.tk';
