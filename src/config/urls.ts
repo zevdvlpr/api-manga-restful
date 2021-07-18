50bfdc263106d4becc0565e62b1aab9f108c8d8c
zevdvlpr <zevdvlpr@gmail.com>
2021-07-18 19:50:39 -0300

    ✨ feat: changing urls

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,3 +1,7 @@
+// MangaLivre
 export const MANGA_LIVRE_SITE_URL = 'https://mangalivre.net';
-export const ANIME_TV_API_URL = 'https://appanimeplus.tk/api-animesbr-10.php';
+
+// AnimeTV
+export const ANIME_TV_AUTH_URL = 'https://auth.appanimeplus.tk';
+export const ANIME_TV_API_URL = 'https://appanimeplus.tk';
 export const ANIME_TV_CDN_URL = 'https://cdn.appanimeplus.tk';
