41ea286d1efd80d7d8986237e989af809f7a97a0
zevdvlpr <zevdvlpr@gmail.com>
2021-07-19 13:08:45 -0300

    ✨ feat: search anime

---
+++ src/pages/api/animes/search.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { getAnimeByName } from '@services/animeTv';
+
+export default async function Category(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    try {
+      const { name, currentPage, pageSize, maxPages } = req.query;
+
+      if (!name) {
+        return res.json({ message: 'The query "name" has not been defined.' });
+      }
+
+      const pagination = {
+        currentPage: Number(currentPage) || 1,
+        pageSize: Number(pageSize) || 12,
+        maxPages: Number(maxPages) || 12,
+      };
+
+      const response = await getAnimeByName(String(name), pagination);
+
+      res.setHeader('x-total-count', response.totalItems);
+
+      return res.send(response);
+    } catch (error) {
+      return res.json({ message: error.message });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method not allowed.' });
+  }
+}
