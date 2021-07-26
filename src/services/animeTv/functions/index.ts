cee73d86ec0f733a535ee225a6ebd3080b5ee6ae
zevdvlpr <zevdvlpr@gmail.com>
2021-07-26 13:38:57 -0300

    ✨ feat: export methods

--- src/services/animeTv/functions/index.ts
+++ src/services/animeTv/functions/index.ts
@@ -5,6 +5,8 @@ export { list } from './list';
 export { allAnimes } from './allAnimes';
 export { animesByCategory } from './animesByCategory';
 export { animesByLatter } from './animesByLatter';
+export { animeBroadcast } from './animeBroadcast';
+export { animeDetails } from './animeDetails';
 export { lastAnimes } from './lastAnimes';
 export { popularAnimes } from './popularAnimes';
 export { searchAnime } from './searchAnime';
